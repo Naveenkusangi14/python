f0bf3b33dbfeb08ca9c4f417d1a00a7d040e613e
NAVEEN KUSANGI <128051619+Naveenkusangi14@users.noreply.github.com>
2024-08-12 20:18:35 +0530

    Update test.py

--- pythoncode-main/test.py
+++ pythoncode-main/test.py
@@ -1,1 +1,12 @@
-print("Hello Have great day ")
+pipeline {
+  agent {
+    docker { image 'node:16-alpine' }
+  }
+  stages {
+    stage('Test') {
+      steps {
+        sh 'node --version'
+      }
+    }
+  }
+}
