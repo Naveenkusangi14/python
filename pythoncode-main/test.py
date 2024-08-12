805bdcb30739c85d9db203127e37cb9cc01ddb12
NAVEEN KUSANGI <128051619+Naveenkusangi14@users.noreply.github.com>
2024-08-12 20:38:44 +0530

    Update test.py

--- pythoncode-main/test.py
+++ pythoncode-main/test.py
@@ -1,12 +1,15 @@
 pipeline {
-  agent {
-    docker { image 'node:16-alpine' }
-  }
-  stages {
-    stage('Test') {
-      steps {
-        sh 'node --version'
-      }
+    agent {
+        docker { 
+            image 'node:16-alpine' 
+            args '-u root:root' // Run as root to avoid permission issues
+        }
     }
-  }
+    stages {
+        stage('Test') {
+            steps {
+                sh 'node --version'
+            }
+        }
+    }
 }
